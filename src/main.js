fb5fc244f0a80c7552aab4c5d3336538c628d1f5
taylor <taylorkanglin@gmail.com>
2021-02-18 14:30:15 +0800

    合并代码

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import router from './router'
 // 引入UI组件样式
 import "element-ui/lib/theme-chalk/index.css";
 
+// 移动端适配
+import 'amfe-flexible/index.js'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
