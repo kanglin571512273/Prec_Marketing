f432a9bea062e06bf602f497e6ea0285d8000b61
taylor <taylorkanglin@gmail.com>
2021-02-18 11:42:04 +0800

    初次提交

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Button,  } from "element-ui";
+Vue.use(Button);
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
