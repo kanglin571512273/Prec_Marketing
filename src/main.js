027ffd310d22195332cc5e38665ccbb96cd3c12c
taylor <taylorkanglin@gmail.com>
2021-02-19 14:02:16 +0800

    合并

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import '@/utils/importFile'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import "@/utils/importFile";
+// 引入css
+import "./assets/css/reset.css";
 
 // 引入UI组件样式
 import "element-ui/lib/theme-chalk/index.css";
 
 // 移动端适配
-import 'amfe-flexible/index.js'
+import "amfe-flexible/index.js";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
--- src/views/login.vue
+++ src/views/login.vue
@@ -8,74 +8,7 @@
       <div class="right-login">
         <div class="login-font">登录</div>
         <div class="login-input">
-          <a-form
-            id="components-form-demo-normal-login"
-            :form="form"
-            class="login-form"
-            @submit="handleSubmit"
-          >
-            <a-form-item>
-              <a-input
-                v-decorator="[
-                  'userName',
-                  {
-                    rules: [{ required: true, message: '请输入手机号/工号' }],
-                    initialValue: 'admin',
-                  },
-                ]"
-                placeholder="请输入手机号/工号"
-                allow-clear
-                size="large"
-                :maxLength="20"
-              >
-                <a-icon
-                  slot="prefix"
-                  type="user"
-                  style="color: rgba(0, 0, 0, 0.25)"
-                />
-              </a-input>
-            </a-form-item>
-            <a-form-item>
-              <a-input-password
-                v-decorator="[
-                  'password',
-                  {
-                    rules: [{ required: true, message: '请输入密码' }],
-                  },
-                ]"
-                type="password"
-                placeholder="请输入密码"
-                size="large"
-              >
-                <a-icon
-                  slot="prefix"
-                  type="lock"
-                  style="color: rgba(0, 0, 0, 0.25)"
-                />
-              </a-input-password>
-            </a-form-item>
-            <a-form-item>
-              <a-checkbox
-                v-decorator="[
-                  'remember',
-                  {
-                    valuePropName: 'checked',
-                    initialValue: true,
-                  },
-                ]"
-              >
-                记住密码
-              </a-checkbox>
-
-              <a-button
-                type="primary"
-                html-type="submit"
-                class="login-form-button"
-              >
-                登录
-              </a-button>
-            </a-form-item>
-          </a-form>
+         
         </div>
       </div>
     </div>
@@ -83,22 +16,19 @@
 </template>
 
 <script>
-import { labelData, customData } from "../utils/data";
-import Vue from "vue";
-import { Form, Input, Icon, Button, Checkbox } from "ant-design-vue";
-Vue.use(Form);
-Vue.use(Input);
-Vue.use(Icon);
-Vue.use(Button);
-Vue.use(Checkbox);
+// import { labelData, customData } from "../utils/data";
+// import Vue from "vue";
+// import { Form, Input, Icon, Button, Checkbox } from "ant-design-vue";
+// Vue.use(Form);
+// Vue.use(Input);
+// Vue.use(Icon);
+// Vue.use(Button);
+// Vue.use(Checkbox);
 export default {
   data() {
     return {
       name: "admin",
     };
-  },
-  beforeCreate() {
-    this.form = this.$form.createForm(this, { name: "normal_login" });
   },
   methods: {
     handleSubmit(e) {
@@ -134,7 +64,7 @@ export default {
 .bg-class {
   width: 40%;
   height: 100%;
-  // background: url("../assets/image/background.png") no-repeat;
+  background: url("../assets/image/background.png") no-repeat;
   background-size: cover;
 }
 .bg-font {
