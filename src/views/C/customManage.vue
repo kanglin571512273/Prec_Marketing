b82da272eb3aad896d9f3e36d3ecba010d74d1a8
zxf <1903044057@qq.com>
2021-03-10 17:13:11 +0800

    客户分析点击搜索数据叠加

--- src/views/C/customManage.vue
+++ src/views/C/customManage.vue
@@ -36,7 +36,7 @@
           @change="getList(true)"
           @clear="getList(true)"
         ></el-input>
-        <div class="sureBtn" @click="getList(null,true)">搜索</div>
+        <div class="sureBtn" @click="getList(true)">搜索</div>
 
         <div class="sureBtn" @click="toAnalysisResoult">确认</div>
         <div class="cancelBtn" @click="cancle">取消</div>
